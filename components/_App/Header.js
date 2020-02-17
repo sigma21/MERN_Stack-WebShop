1fa2e3d8a757ce4f421745de995247f78acf2099
sigma21 <semihkocakel@hotmail.com>
2020-02-17 13:18:53 +0100

    app layout, header, active links, route change progress bar

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,87 @@
+import { Menu, Container, Image, Icon } from "semantic-ui-react";
+import Link from "next/link";
+import Router, { useRouter } from "next/router"; //to detect the route we are in
+import NProgress from "nprogress"; //to display a progress bar above header when changing routes
+
+//timing the progress bar from the beginning of the click till the new route
+Router.onRouteChangeStart = () => NProgress.start() 
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done() // in case there is an error with the destination route
+
+
 function Header() {
-  return <>Header</>;
+  const router = useRouter(); //gives us an object with pathname etc.
+  const user = false; //dummy data to determine which tabs will be displayed within the header
+
+  //helper function to determine the path
+  function isActive(route) {
+    return route === router.pathname;
+  }
+
+  return (
+    <Menu fluid id="menu" inverted>
+      <Container text>
+        <Link href="/">
+          <Menu.Item header>
+            <Image
+              size="mini"
+              src="/static/logo.svg"
+              style={{ marginRight: "1em" }}
+            ></Image>
+            WebShop
+          </Menu.Item>
+        </Link>
+
+        <Link href="/cart">
+          <Menu.Item header active={isActive("/cart")}>
+            <Icon name="cart" size="large"></Icon>
+            Cart
+          </Menu.Item>
+        </Link>
+
+        {user && (
+          <Link href="/create">
+            <Menu.Item header active={isActive("/create")}>
+              <Icon name="add square" size="large"></Icon>
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+
+        {user ? (
+          <>
+            <Link href="/account">
+              <Menu.Item header active={isActive("/account")}>
+                <Icon name="user" size="large"></Icon>
+                Account
+              </Menu.Item>
+            </Link>
+
+            <Menu.Item header>
+              <Icon name="sign out" size="large"></Icon>
+              Logout
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Link href="/login">
+              <Menu.Item header active={isActive("/login")}>
+                <Icon name="sign in" size="large"></Icon>
+                Login
+              </Menu.Item>
+            </Link>
+
+            <Link href="/signup">
+              <Menu.Item header active={isActive("/signup")}>
+                <Icon name="signup" size="large"></Icon>
+                Signup
+              </Menu.Item>
+            </Link>
+          </>
+        )}
+      </Container>
+    </Menu>
+  );
 }
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import App from "next/app";
+import Layout from "../components/_App/Layout";
 
+
+//where the whole page displayed. wrapping around layout to implement the design layout from layout.js
 class MyApp extends App {
   render() {
     const { Component } = this.props;
-    return <Component />;
+    return (
+      <Layout>
+        <Component />;
+      </Layout>
+    );
   }
 }
 
