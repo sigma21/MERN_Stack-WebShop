0acd8e98da747af7427d93259bbc340a7733ffa9
sigma21 <semihkocakel@hotmail.com>
2020-02-25 15:44:23 +0100

    style product page, baseUrl helper

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
-import ProductList from "../components/Index/ProductList"
-
+import ProductList from "../components/Index/ProductList";
+import baseUrl from "../utils/baseUrl";
 
 function Home({ products }) {
   //useEffect hook is used as a side effect, when sth is done outside this function & get used inside
@@ -15,14 +15,13 @@ function Home({ products }) {
   //   console.log(response.data);
   // }
 
-
-  return <ProductList products={products}/>
+  return <ProductList products={products} />;
 }
 
 //with below function we are fetching the json data in the server instead of client
 Home.getInitialProps = async () => {
   //fetch data on the server
-  const url = "http://localhost:3000/api/products";
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   //return response data as an object(because props is an object)
   //this object will be merged with the existing props (if there is any)
--- pages/product.js
+++ pages/product.js
@@ -1,16 +1,23 @@
 import axios from "axios";
+import ProductSummary from "../components/Product/ProductSummary";
+import ProductAttributes from "../components/Product/ProductAttributes";
+import baseUrl from "../utils/baseUrl";
 
-function Product({product}) {
-  console.log(product)
-  return <>product</>;
+function Product({ product }) {
+  return (
+    <>
+      <ProductSummary {...product} />
+      <ProductAttributes {...product} />
+    </>
+  );
 }
 
 Product.getInitialProps = async ({ query: { _id } }) => {
   //ctx nan object gives details about route etc.
-  const url = `http://localhost:3000/api/product`;
+  const url = `${baseUrl}/api/product`;
   const payload = { params: { _id } };
   const response = await axios.get(url, payload);
-  return {product: response.data}
+  return { product: response.data };
 };
 
 export default Product;
